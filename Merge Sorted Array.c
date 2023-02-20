bab4f6bcc3a9c5a122cd7270cfb97ca01c25abba
Meet Joshi <91955713+Meetjoshi2602@users.noreply.github.com>
2023-02-20 17:13:41 +0530

    Add files via upload

---
+++ Merge Sorted Array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int nums1= arr[3],nums2=arr[3];
+int i=3;
+{
+    printf("nums1=",%d);
+    printf("nums2=",%d);
+   // from here we can short arry using for loop
+    for(int nums1;nums1<=[3];num1++)
+    {
+        for(int nums2;nums2<=[3];nums2++)
+        {
+            printf("Output:",%d);
+        }
+    }
+}
+
---
+++ Shuffle the Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+int arr[];
+int n;
+{
+     printf("nums:");
+     scanf("%d",&n);
+{
+    //Here we can use for loopa and then take ans using index number.
+}
+
+
+}
